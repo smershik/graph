383f3ad241a97ce44fe752378a58ab578613d4d9
smershik <smershik103@yandex.ru>
2017-01-23 18:32:11 +0300

    test commit

---
+++ scripts/main.js
@@ -0,0 +1,118 @@
+"use strict"
+
+function Graph(){
+	var nodes = {
+		'1': {
+			'2':9,
+			'3':10,
+			'4':7
+		},
+		'2': {
+			'1':9,
+			'4':2,
+			'3':14,
+			'5':15
+		},
+		'3': {
+			'1':10,
+			'2':14,
+			'4':21
+		},
+		'4': {
+			'1':7,
+			'2':2,
+			'3':21
+		},
+		'5': {
+			'2':15
+		}
+	};
+
+	var routes;
+
+	var searchRoutes = function (startNode,finishNode,openNodes = []) {
+		openNodes.push(+startNode);
+		if(nodes[startNode][finishNode]!==undefined){
+			openNodes.push(+finishNode);
+			var distance = 0;
+			for(var i=0;i<openNodes.length-1; i++){
+				distance+=nodes[openNodes[i]][openNodes[i+1]];
+			}
+			routes.push(distance);
+		} else {
+			for(var i in nodes[startNode]){
+				if(openNodes.indexOf(+i, 0)==-1) {
+					searchRoutes(i,finishNode,openNodes.slice(0, openNodes.length+1));
+				}
+			}
+		}
+	};
+
+	this.searchMinRoute = function(startNode,finishNode){
+		if (nodes[startNode]==undefined||nodes[finishNode]==undefined) return 'not existing node';
+		if (startNode==finishNode) return 0;
+		routes = [];
+		searchRoutes(startNode,finishNode);
+		return Math.min.apply(null, routes);
+	};
+
+	this.addNode = function(name, links){
+		nodes[name] = links;
+		for(var i in nodes[name]){
+			nodes[i][name] = links[i];
+		}
+	}
+
+	this.removeNode = function(name){
+		delete nodes[name];
+		for(var i in nodes){
+			if (nodes[i][name]!==undefined) delete nodes[i][name];
+		}
+		console.log(nodes);
+	}
+
+	this.nodesAdapter = function(){
+		var result = [];
+		for (var i in nodes){
+			result.push({id: +i,label:'Node '+i});
+		}
+		return result;
+	}
+
+	this.edgesAdapter = function(){
+		var result = [];
+		for (var links in nodes){
+			for (var i in nodes[links]){
+				var check = false; 
+				result.forEach( function(item) {
+					if(item['from']==i&&item['to']==links) check = true;
+				});
+				if(!check)
+					result.push({from: +links, to: +i, label: nodes[links][i],font:{align: 'middle'}});
+			}
+		}
+		return result;
+	}
+	
+	}
+
+		
+//need visualisation
+//
+
+var g = new Graph();
+console.log(g.searchMinRoute(1,5));
+
+var nodes = g.nodesAdapter();
+
+  // create an array with edges
+var edges = g.edgesAdapter();
+
+  // create a network
+var container = document.getElementById('mynetwork');
+var data = {
+  nodes: nodes,
+  edges: edges
+  };
+var options = {};
+var network = new vis.Network(container, data, options);
---
+++ scripts/tests.js
@@ -0,0 +1,24 @@
+describe("returns minimal route",function() {
+	// it("from 3 to 5", function () {
+	// 	assert.equal(g.searchMinRoute(3,5),29);
+	// });
+	// it("from 1 to 5", function () {
+	// 	assert.equal(g.searchMinRoute(1,5),24);
+	// });
+	// it("from 4 to 5", function () {
+	// 	assert.equal(g.searchMinRoute(4,5),17);
+	// });
+	// it("from 5 to 3", function () {
+	// 	assert.equal(g.searchMinRoute(5,3),29);
+	// });
+	// it("from 1 to 2", function () {
+	// 	assert.equal(g.searchMinRoute(1,2),9);
+	// });
+	// it("from 1 to 1", function () {
+	// 	assert.equal(g.searchMinRoute(1,1),0);
+	// });
+	// it("from 1 to 999", function () {
+	// 	assert.equal(g.searchMinRoute(1,999),'not existing node');
+	// });
+
+});
